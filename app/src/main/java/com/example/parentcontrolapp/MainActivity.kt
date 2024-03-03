7a71ea910771a2d52954de5e8c209a8fc41395b3
resqiar <resqiagengr@gmail.com>
2024-03-04 04:51:55 +0700

    chore: remove unused import

--- app/src/main/java/com/example/parentcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/parentcontrolapp/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.example.parentcontrolapp
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.parentcontrolapp.ui.screens.InstalledAppsList
 import com.example.parentcontrolapp.ui.theme.ParentControlAppTheme
 import com.example.parentcontrolapp.utils.getInstalledApps
